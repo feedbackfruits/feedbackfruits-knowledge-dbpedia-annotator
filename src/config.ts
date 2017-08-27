6fda5a47addebfcfc69ea05354360e8b4210e585
Steffan Sluis <steffansluis@gmail.com>
2017-08-27 03:04:58 +0200

    Use typescript

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+require('dotenv').load({ silent: true });
+
+const {
+  NAME = 'feedbackfruits-knowledge-dbpedia-annotator',
+  KAFKA_ADDRESS = 'tcp://localhost:9092',
+  INPUT_TOPIC = 'staging_updates',
+  OUTPUT_TOPIC = 'staging_update_requests',
+  DBPEDIA_SPARQL_ENDPOINT = 'http://dbpedia.org/sparql'
+} = process.env;
+
+export {
+  NAME,
+  KAFKA_ADDRESS,
+  INPUT_TOPIC,
+  OUTPUT_TOPIC,
+  DBPEDIA_SPARQL_ENDPOINT,
+}
